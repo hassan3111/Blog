b9ab972f08fcfd865d423a88cf3da32a3c61c6a2
hassan3111 <ali.hassan@student.moringaschool.com>
2021-11-14 13:08:07 +0300

    add modification to config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 class Config:
     SECRET_KEY = 'DZO3PQkI_yJy8e-2u_7Feg'
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://access:34567@localhost/hassan'
+    app.config["SQLALCHEMY_DATABASE_URI"]= 'postgresql+psycopg2://access:34567@localhost/hassan'
     UPLOADED_PHOTOS_DEST = 'app/static/photos'
     QUOTES_URL = 'http://quotes.stormconsultancy.co.uk/random.json'
     MAIL_SERVER = 'smtp.googlemail.com'
