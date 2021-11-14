1b4673f7eff03d733d228f9a3a9eb1c69e0cd8cb
hassan3111 <ali.hassan@student.moringaschool.com>
2021-11-14 13:47:46 +0300

    set track  modification in config.py

--- config.py
+++ config.py
@@ -13,7 +13,7 @@ class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = True
 
 class ProdConfig(Config):
-  SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL?sslmode=require')
+  SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 
 class DevConfig(Config):
 
