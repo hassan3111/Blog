3c2cc0fa39d96cf54b24ab82860c98b34973cb56
hassan3111 <ali.hassan@student.moringaschool.com>
2021-11-14 13:42:46 +0300

    set track  modification in config.py

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ class Config:
     MAIL_USE_TLS = True
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
 
 class ProdConfig(Config):
   SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL?sslmode=require')
