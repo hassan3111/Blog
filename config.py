2762f9943b07d04a3f33334a7df4f5f80e513f35
hassan3111 <ali.hassan@student.moringaschool.com>
2021-11-14 13:01:56 +0300

    initial deployment

--- config.py
+++ config.py
@@ -13,7 +13,9 @@ class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 class ProdConfig(Config):
-      SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") 
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+
+    
 class DevConfig(Config):
 
     DEBUG = True   
